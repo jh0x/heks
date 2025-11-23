adb08f256557266cc133163e3dfff51672ea9f5e
Joanna Hulboj <j@hulboj.org>
2025-11-23 21:25:02 +0000

    Add option to build march=native to cmake

--- cmake/variables.cmake
+++ cmake/variables.cmake
@@ -8,6 +8,7 @@ if(PROJECT_IS_TOP_LEVEL)
     option(fast_hex_DEVELOPER_MODE "Enable developer mode" OFF)
     option(BUILD_SHARED_LIBS "Build shared libs." OFF)
     option(fast_hex_USE_NAMESPACE "Use heks namespace for fast_hex library" ON)
+    option(fast_hex_ENABLE_MARCH_NATIVE "Enable -march=native" ON)
 endif()
 
 # ---- Suppress C4251 on Windows ----
