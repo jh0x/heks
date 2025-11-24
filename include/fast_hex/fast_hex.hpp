746807c9dfbc4655bbd5ade7660f824569c0cad8
Joanna Hulboj <j@hulboj.org>
2025-11-24 18:48:13 +0000

    Added few comments

--- include/fast_hex/fast_hex.hpp
+++ include/fast_hex/fast_hex.hpp
@@ -29,10 +29,12 @@ FAST_HEX_NAMESPACE_OPEN
 // Length of raw data in bytes (e.g "Test123" is 7 bytes)
 enum class RawLength : size_t;
 
-// Scalar look-up table version. len is number of dest bytes (1/2 the size of src).
+// Scalar look-up table version (two chars at a time). Uses a single lookup table, but requires a shift.
+// [0xAB, 0xCD] -> [a = 0xA, b = 0xB] -> dest = (a << 4) | b = 0xAB
 FAST_HEX_EXPORT void decodeHexLUT(uint8_t * FAST_HEX_RESTRICT dest, const uint8_t * FAST_HEX_RESTRICT src, RawLength len);
 
-// Optimized scalar look-up table version (avoids a shift). len is number of dest bytes (1/2 the size of src).
+// Scalar look-up table version (two chars at a time). Uses two lookup tables to avoid the shift.
+// [0xAB, 0xCD] -> [a = 0xA, b = 0xB] -> dest = a | b = 0xAB
 FAST_HEX_EXPORT void decodeHexLUT4(uint8_t * FAST_HEX_RESTRICT dest, const uint8_t * FAST_HEX_RESTRICT src, RawLength len);
 
 #if defined(__AVX2__)
@@ -44,6 +46,8 @@ FAST_HEX_EXPORT void decodeHexVec(uint8_t * FAST_HEX_RESTRICT dest, const uint8_
 // Encode src bytes (e.g. "Test123") into dest hex string (e.g. "54657374313233")
 
 // Scalar version. len is number of src bytes. dest must be twice the size of src.
+// [0xAB, 0xCD] -> [hi = 0xA, lo = 0xB] -> [dest[0] = 'A', dest[1] = 'B']
+//                 [hi = 0xC, lo = 0xD] -> [dest[2] = 'C', dest[3] = 'D']
 FAST_HEX_EXPORT void encodeHexLower(uint8_t * FAST_HEX_RESTRICT dest, const uint8_t * FAST_HEX_RESTRICT src, RawLength len);
 FAST_HEX_EXPORT void encodeHexUpper(uint8_t * FAST_HEX_RESTRICT dest, const uint8_t * FAST_HEX_RESTRICT src, RawLength len);
 
@@ -51,6 +55,13 @@ FAST_HEX_EXPORT void encodeHexUpper(uint8_t * FAST_HEX_RESTRICT dest, const uint
 #if defined(__AVX__)
 // Fast specialized paths for fixed-size encoding
 // Encode exactly 8 bytes (source) into 16 hex characters (dest)
+// [0xAB, 0xCD, 0xEF, 0x12, 0x34, 0x56, 0x78, 0x9A]
+//     -> hi = [0xA, 0xC, 0xE, 0x1, 0x3, 0x5, 0x7, 0x9]
+//     -> lo = [0xB, 0xD, 0xF, 0x2, 0x4, 0x6, 0x8, 0xA]
+//         -> nibs = [hi[0], lo[0], hi[1], lo[1], ...]
+//                 = [0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9]
+//             -> LUT lookup
+//                 -> result = [A, B, C, D, E, F, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 FAST_HEX_EXPORT void encodeHex8LowerFast(uint8_t * FAST_HEX_RESTRICT dest, const uint8_t * FAST_HEX_RESTRICT src);
 FAST_HEX_EXPORT void encodeHex8UpperFast(uint8_t * FAST_HEX_RESTRICT dest, const uint8_t * FAST_HEX_RESTRICT src);
 #endif
